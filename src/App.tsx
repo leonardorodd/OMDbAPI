24fb58557547c2746791210f21c5993fe44a77d0
leonardorodd <leo_rodrigues128@hotmail.com>
2023-07-28 20:24:46 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import GlobalStyle from './styles/global';
 
 const src: React.FC = () => {
   return (
-    <BrowserRouter basename='/OMDbAPI'>
+    <BrowserRouter>
       <ToastContainer theme="dark" autoClose={3000}/>
       <Routes/>
       <GlobalStyle />
